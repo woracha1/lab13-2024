5b2c23cafd77e2283f24f3055019276f1351108e
woracha1 <zz814170@gmail.com>
2024-12-29 04:06:43 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,46 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    
+    double sum = 0 ;
+    for(int i = 0;i<N;i++){
+        sum += A[i] ;
+    }
+    
+    B[0] = sum / N ;
+    
+    double sumsd = 0;
+    for(int i = 0;i<N;i++){
+        sumsd += pow(A[i]-B[0],2) ;
+    }
+    B[1] = sqrt(sumsd/(N));
+    
+    double pro = 1.0 ;
+    for(int i = 0;i<N;i++){
+        pro *= A[i] ;
+    }
+    B[2] = pow(pro,(1.0/N));
+    
+    double sumhmn = 0 ;
+    for(int i = 0;i<N;i++){
+        sumhmn += 1/A[i] ;
+    }
+    B[3] = N / sumhmn ;
+    
+    double max = A[0];
+    for(int i = 1;i<N;i++){
+        if(A[i]>max)
+            max = A[i] ;
+    }
+    B[4] = max;
+    
+    double min = A[0];
+    for(int i = 1;i<N;i++){
+        if(A[i]<min)
+            min = A[i] ;
+    }
+    B[5] = min;
+    
+}
