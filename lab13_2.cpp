614c114305514bed79e1ad95a4a977df3030b472
woracha1 <zz814170@gmail.com>
2024-12-29 04:18:44 +0700

    lab13_2 completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------" << endl ;
+    for(int i = 0 ;i<N;i++){
+        cout << "|" ;
+        for(int u = 0;u<M;u++){
+            if(image[i][u]==1){
+                cout << "*" ;
+            }
+            else{
+                cout<<" " ;
+            }
+        }
+        cout<<"|" << endl ;
+    }
+    cout << "------------------------------------------------------------------------" << endl ;
+}
+
+void updateImage(bool image[N][M],int s,int x,int y){
+    for(int i = x-s+1;i<x+s;i++){
+        for(int j = y-s+1;j<y+s;j++){
+            if(i<N && j<M && i>=0 && j>=0 && sqrt(pow(i-x,2)+pow(j-y,2))<=s-1) image[i][j]=1 ;
+        }
+    }
+}
